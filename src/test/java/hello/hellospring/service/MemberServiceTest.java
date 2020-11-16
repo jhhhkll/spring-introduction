461cee65423e4b7ba9eaf0d6c47838e03ff8c132
손기문 <jhhhkll@gmail.com>
2020-11-16 20:09:59 +0900

    회원 관

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,75 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    //  의존성 주입
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void 회원가입(){
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));     //      예외가 발생해서 e로 받아야함
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");     //      예외 메시지가 원하는 메시지인지 검증
+
+        /*  try-catch로 예외 발생하는지 확인
+        try{
+            memberService.join(member2);
+            fail("예외 처리 안됨");
+        } catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+
+         */
+
+    }
+}
